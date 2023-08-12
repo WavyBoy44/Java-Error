a8677912c418ccb2aa81dc182f55a195cb5fa5b2
Fabian Mendoza-Cisneros <47339451+WavyBoy44@users.noreply.github.com>
2023-08-12 00:43:58 -0400

    Created java program with errors.

---
+++ src/Errors.java
@@ -0,0 +1,14 @@
+public class Errors {
+    public static void main(String[] args){
+        integer one = 0;
+        int two = 1;
+        String s = "";
+        String x;
+        int 1 = 0;
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(s);
+        System.out.println(x);
+        System.out.println(1);
+    }
+}
